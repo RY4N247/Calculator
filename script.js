09d624fb7cc2f98d3fb0613a68ca940e26c7082b
Ryan Bramwell CESM2022 <ryan.bramwell.2022@uni.strath.ac.uk>
2024-04-19 22:32:45 +0100

    Improved code structure and less listeners for improved memory

--- script.js
+++ script.js
@@ -1,39 +1,51 @@
 function myAdd(n1, n2) {
-    return n1 + n2;
+  return n1 + n2;
 }
 
 function mySubtract(n1, n2) {
-    return n1 - n2;
+  return n1 - n2;
 }
 
-function myMultiply(n1 ,n2) {
-    return n1 * n2;
+function myMultiply(n1, n2) {
+  return n1 * n2;
 }
 
 function myDivide(n1, n2) {
-    return n1 / n2;
+  if (n2 === 0) {
+      return "Error: Division by zero";
+  }
+  return n1 / n2;
 }
 
 
-let current= ""; 
+function main() {
+  let current = "";
+  const touchpad = document.querySelector(".touchpad");
 
+  //add event listener to touchpad for button presses 
+  touchpad.addEventListener("click", handleInput);
 
-const buttons = document.querySelectorAll('.button');
+  function handleInput(event) {
+    const output = document.getElementById("output");
+    const inputValue = event.target.textContent;
+    current = current + inputValue;
 
+    output.innerHTML = current;
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    //console.log(button.textContent);
-    //document.getElementById("output").innerHTML = button.textContent;
-    current = current + button.textContent;
-    document.getElementById("output").innerHTML = current;
-    console.log(current);
-  });
-});
-
-function operations(current){
     
+    
+  
+   
+  
+  
+  
+  
+  }
+
+
+
+
 
 }
 
-
+main();
